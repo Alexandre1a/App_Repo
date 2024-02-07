f0e5da12a9a5f7453eec3f6b29c42b6af9ac5bff
Alexandre1a <Mateialex@outlook.fr>
2024-02-07 22:02:55 +0100

    Update V0.0.3

--- Launcher (Deprecated)/V0.py
+++
@@ -1,8 +0,0 @@
-import tkinter as tk
-
-root = tk.Tk()
-
-button1 = tk.Button(root, text="Ouvrir Programme 1")
-button1.pack()
-
-root.mainloop()
--- Site (Deprecated)/Readme.md
+++
@@ -1,1 +0,0 @@
-I think I won't use a website and just release all the updates/manifest on Github
