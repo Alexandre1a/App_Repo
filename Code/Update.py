2f00c8d9b474d65d2855d43ee26b9672c6259428
Alexandre1a <Mateialex@outlook.fr>
2024-02-08 21:11:38 +0100

    V0.0.3

--- Code/Update.py
+++ Code/Update.py
@@ -5,13 +5,15 @@ import requests
 fichier_local = "manifest_OLD.json"
 
 # Téléchargement du fichier JSON distant
-url_fichier_json = "https://raw.githubusercontent.com/Alexandre1a/App_Repo/Test/Update/manifest.json"
+url_fichier_json = "https://raw.githubusercontent.com/Alexandre1a/App_Repo/Test/Update/manifest.json?token=GHSAT0AAAAAACN52LZKW2WLYG2EN6EJRAUMZOD6SMQ"
 response = requests.get(url_fichier_json)
 
 # Si le téléchargement du fichier distant est OK
 if response.status_code == 200:
     # Décodage du fichier JSON distant
     contenu_json_distant = json.loads(response.content)
+    with open("manifest.json", "wb") as fichier:
+        fichier.write(response.content)
 
     # Lecture du fichier JSON local
     with open(fichier_local, "r") as fichier:
