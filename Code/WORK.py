e96fb7b6fe54136a663252d66bfb8c3e362f3d76
Alexandre1a <107309823+Alexandre1a@users.noreply.github.com>
2024-03-14 09:51:03 +0100

    NEW UPDATE !!! (#3)
    * Update V0.0.2
    
    * Update V0.0.3
    
    * Update manifest.json
    
    Test Manifest
    
    * Update README.md
    
    * V0.0.3
    
    * Updated the URL with the public repo
    
    * Added the rename thing and the remove thing to have just one manifest :)
    
    * Previous commit, same comment
    
    * Added a little popup ( WIP, subject to changes )
    
    I added a little popup if the Update.py detect a newer JSON. For the next update I will need to add a system to make the popup display the Updates tab. And then a complete Update system with a bigget JSON file !

--- Code/WORK.py
+++ Code/WORK.py
@@ -1,12 +1,17 @@
 import tkinter as tk
 from tkinter import ttk
+from Update import * # Pour importer la fonction de MAJ. ATTENTION : Si importé tel quel, lancera le processus de vérification JSON automatiquement au démarage !
+from lib im
 
 class Application(tk.Tk):
     def __init__(self):
         super().__init__()
 
         self.title("Alexandre_1a's LAUNCHER") # Définit la taille et le nom de l'app
-        self.geometry("400x500")
+        self.geometry("600x500")
+        self.resizable(width=0,height=0) # size change Not allowed 
+        self.configure(bg='red')
+        self.geometry("400x500"
 
         self.notebook = ttk.Notebook(self) # Permet de définir le fait qu'il y ait des onglets
         self.notebook.pack(fill=tk.BOTH, expand=True)
@@ -20,6 +25,11 @@ class Application(tk.Tk):
         self.notebook.add(self.page2, text="Paramètres")
         self.notebook.add(self.page3, text="Updates")
         self.notebook.add(self.page4, text="About")
+
+
+    if manifest=="=" :
+        print("Update Available")
+        popupmsg("A new update is available, go to the update page to get the new version !")
 '''
         self.key_to_bind = "u" # Important : permet de changer le keybind par défaut
         self.bind(f"<KeyPress-{self.key_to_bind}>", self.page2.show_updates)  # Associe la touche définie par la variable à la fonction show_credits
@@ -28,6 +38,15 @@ class MainMenu(tk.Frame): # Affiche le menu principal
     def __init__(self, parent):
         super().__init__(parent)
 
+
+        label = tk.Label(self, text="Bienvenue Sur Le Menu Principal")
+        label.pack(padx=10, pady=10)
+
+        button_middle = tk.Button(self, text="Test !", command=self.play_game)
+        button_middle.pack(side=tk.TOP, pady=20)
+
+        button_bottom = tk.Button(self, text="Quitter", command=self.quit_application) # Permet de faire un boutton quitter ( à incorporer partout !)
+        button_bottom.pack(side=tk.BOTTOM, pady=10)
         label = tk.Label(self, text="Bienvenue dans le Menu Principal")
         label.pack(padx=10, pady=10)
 
@@ -37,6 +56,12 @@ class MainMenu(tk.Frame): # Affiche le menu principal
         button_bottom = tk.Button(self, text="Paramètres", command=self.show_settings)
         button_bottom.pack(side=tk.BOTTOM, pady=10)
 
+    def quit_application(self):
+        app.destroy()
+        print("App Quitté")
+
+    def play_game(self):
+        print("Test Is OK")
     def play_game(self):
         print("Test Triggered sucessfully !")
 
@@ -63,8 +88,13 @@ class SettingsPage(tk.Frame):
         keybind_entry.pack(pady=5)'''
 
         # Boutons pour changer la résolution et le keybind
+
+        button_400 = tk.Button(self, text="Passer en 400*400", command=self.change_geometry_400)
+        button_400.pack(pady=5)
+
         button_720p = tk.Button(self, text="Changer en 720p", command=lambda: self.change_resolution("720p"))
         button_720p.pack(pady=5)
+
 
         button_1080p = tk.Button(self, text="Changer en 1080p", command=lambda: self.change_resolution("1080p"))
         button_1080p.pack(pady=5)
@@ -75,11 +105,24 @@ class SettingsPage(tk.Frame):
         button_save = tk.Button(self, text="Sauvegarder", command=lambda: self.save_keybind(keybind_entry))
         button_save.pack(pady=5)'''
 
+
+        button_bottom = tk.Button(self, text="Quitter", command=self.quit_application) # Permet de faire un boutton quitter ( à incorporer partout !)
+        button_bottom.pack(side=tk.BOTTOM, pady=10)
+
         button_middle = tk.Button(self, text="Retour au Menu", command=self.show_main_menu)
         button_middle.pack(pady=20)
 
+
         button_bottom = tk.Button(self, text="Updates", command=self.show_updates)
         button_bottom.pack(side=tk.BOTTOM, pady=10)
+
+
+        button_middle = tk.Button(self, text="Retour au Menu", command=self.show_main_menu)
+        button_middle.pack(side=tk.BOTTOM, pady=10)
+
+    def quit_application(self):
+        app.destroy()
+        print("App Quitté")
 
         button_bottom = tk.Button(self, text="Quitter", command=self.quit_application) # Permet de faire un boutton quitter ( à incorporer partout !)
         button_bottom.pack(side=tk.RIGHT, pady=100, padx=10)
@@ -87,17 +130,27 @@ class SettingsPage(tk.Frame):
     def quit_application(self):
         app.destroy()
         print("App quitté")
+
         
     def show_main_menu(self):
         self.app.notebook.select(0)  # Sélectionne la page du menu principal
         print("Menu Principal Affiché")
 
+    def show_updates(self):
+        self.app.notebook.select(2)  # Sélectionne la page des crédits
+        print("Updates Affiché")
+
+    def change_geometry_400(self):
+        print("Changer la géométrie en 400*400")
+        self.geometry("400x400")
+                      
     def show_updates(self, event=None):
         self.app.notebook.select(2)  # Sélectionne la page des crédits
         print("Updates Affiché")
 
     def change_resolution(self, new_resolution):
         print(f"Changer la résolution en {new_resolution}")
+
 '''
     def change_keybind(self, new_keybind):
         self.app.unbind(f"<KeyPress-{self.app.key_to_bind}>")  # Désassocie l'ancien keybind
@@ -111,6 +164,31 @@ class SettingsPage(tk.Frame):
 class UpdatePage(tk.Frame):
     def __init__(self, parent):
         super().__init__(parent)
+
+        label = tk.Label(self, text="Page Des Mises à jour")
+        label.pack(padx=10, pady=10)
+
+        button_bottom = tk.Button(self, text="Quitter", command=self.quit_application)
+        button_bottom.pack(side=tk.BOTTOM, pady=10)
+
+        button_bottom = tk.Button(self, text="About", command=self.show_about)
+        button_bottom.pack(side=tk.BOTTOM, pady=10)
+
+        button_middle = tk.Button(self, text="Retour au Menu", command=self.show_main_menu)
+        button_middle.pack(side=tk.BOTTOM, pady=10)
+
+
+    def show_main_menu(self):
+        app.notebook.select(0)  # Sélectionne la page du menu principal
+        print("Menu Principal Affiché")
+
+    def quit_application(self):
+        app.destroy()
+        print("App Quitté")
+
+    def show_about(self):
+        app.notebook.select(3)
+        print("About Affiché")
 
         label = tk.Label(self, text="Page des Mises à jour")
         label.pack(padx=10, pady=10)
@@ -132,6 +210,14 @@ class AboutPage(tk.Frame):
         super().__init__(parent)
 
         label = tk.Label(self, text="Créé par : Alexandre_1a")
+        label.pack(side=tk.TOP, pady=1)
+
+        label = tk.Label(self, text="2023-2024 Alexandre_1a")
+        label.pack(side=tk.TOP, pady=1)
+
+        label = tk.Label(self, text="Made With Python (Tkinter) and some programmer magic")
+        label.pack(side=tk.TOP, pady=1)
+                      
         label.pack(padx=10, pady=10)
         label = tk.Label(self, text="2023-2024 Alexandre_1a")
         label.pack(padx=10, pady=5)
@@ -139,9 +225,17 @@ class AboutPage(tk.Frame):
         button_middle = tk.Button(self, text="Retour au Menu", command=self.show_main_menu)
         button_middle.pack(pady=20)
 
+
         button_bottom = tk.Button(self, text="Quitter", command=self.quit_application) # Permet de faire un boutton quitter ( à incorporer partout !)
         button_bottom.pack(side=tk.BOTTOM, pady=10)
 
+
+        button_middle = tk.Button(self, text="Retour Au Menu", command=self.show_main_menu)
+        button_middle.pack(side=tk.BOTTOM, pady=10)
+
+    def show_main_menu(self):
+        app.notebook.select(0)  # Sélectionne la page du menu principal
+        print("Menu Principal Affiché")
     def show_main_menu(self):
         app.notebook.select(0)  # Sélectionne la page du menu principal
 
