a4cf853a11233d7afdcb5c8bef0801395ce1a002
Alex <Mateialex@outlook.fr>
2024-03-20 17:59:53 +0100

    First set to the rework of the Update System
    I added multiple thing to Update.py like adding functions to add like small modules. I also added a system to verify if the file is already prensent to sort of "save" badwitch ( even if the manifest is like 3 bytes ). This is the first step of a updated update system so the end user don't have to download every new version from github ! Next step, adding stuff to the manifest so he knows where to pull the files from !

--- Code/Update.py
+++ Code/Update.py
@@ -1,51 +1,81 @@
 import json
 import requests
 import os
+import os.path
 from urllib import request
 
 # Définition de la variable
-manifest=str(".")
+manifest=str("-")
 # Définition du chemin du fichier JSON local
-fichier_local = "manifest_OLD.json"
+fichier_local = "manifest_current.json"
 
-# Téléchargement du fichier JSON distant
-url_fichier_json = "https://raw.githubusercontent.com/Alexandre1a/App_Repo/Test/manifest.json"
-response = requests.get(url_fichier_json)
-def internet_on():
-    try:
-        request.urlopen('http://216.58.192.142', timeout=1)
-        return True
-    except request.URLError as err: 
-        return False
-
-# Si le téléchargement du fichier distant est OK
-if response.status_code == 200 and internet_on==True:
+def File_Download():   
+    # Téléchargement du fichier JSON distant
+    url_fichier_json = "https://raw.githubusercontent.com/Alexandre1a/App_Repo/Production/manifest.json"
+    response = requests.get(url_fichier_json)
+    print("Fichier téléchargé !")
     # Décodage du fichier JSON distant
     contenu_json_distant = json.loads(response.content)
     with open("manifest.json", "wb") as fichier:
         fichier.write(response.content)
+        print("Fichier écrit !")
 
-    # Lecture du fichier JSON local
-    with open(fichier_local, "r") as fichier:
-        contenu_json_local = json.load(fichier)
+def internet_on():
+    try:
+        request.urlopen('https://google.com', timeout=10)
+        return True
+        print("Internet ok ! (en principe)")
+    except request.URLError as err: 
+        return False
+        print("Connection échouée !")
 
-    # Extraction des versions
-    version_telechargee = contenu_json_distant["version"]
-    version_locale = contenu_json_local["version"]
+def Check_Changes():
+    # Si le téléchargement du fichier distant est OK
+    if internet_on()==True:
+        # Lecture du fichier JSON local
+        with open(fichier_local, "r") as fichier:
+            contenu_json_local = json.load(fichier)
+            print("Fichier lu !")
 
-    # Comparaison des versions
-    if version_telechargee > version_locale:
-        print("+")
-        manifest="+"
-        os.remove("manifest_OLD.json")
-        os.rename("manifest.json", "manifest_OLD.json")
-    elif version_telechargee < version_locale:
-        print("-")
-        manifest="-"
+        # Extraction des versions + Définition de là où est le fichier distant
+        url_fichier_json = "https://raw.githubusercontent.com/Alexandre1a/App_Repo/Production/manifest.json"
+        response = requests.get(url_fichier_json)
+        contenu_json_distant = json.loads(response.content)
+        version_telechargee = contenu_json_distant["version"]
+        version_locale = contenu_json_local["version"]
+        print("Versions extraites des JSON !")
+       
+        # Comparaison des versions
+        if version_telechargee > version_locale:
+            print("+")
+            manifest="+"
+        elif version_telechargee < version_locale:
+            print("-")
+            manifest="-"
+        else:
+            print("=")
+            manifest="="
+        print("Fichiers comparés !")
     else:
-        print("=")
-        manifest="="
-        os.remove("manifest_OLD.json")
-        os.rename("manifest.json", "manifest_OLD.json")
-else:
-    print("Erreur lors du téléchargement du fichier JSON distant (Vérifiez votre connection internet !)")
+        print("Erreur lors de la comparaison des version ! (Erreur inconue !)")
+
+
+def Update():
+    if os.path.isfile("manifest.json"):
+        print("Fichier présent !")
+        Check_Changes()
+        if manifest== "+":
+            print("Update Available, starting the upgrade process !")
+            
+        # Removes the old manifest and rename the new one for the next update 
+        os.remove("manifest_current.json")
+        os.rename("manifest.json", "manifest_current.json")
+    else:
+        print("Fichier non présent donc téléchargé !")
+        File_Download()
+
+Update()
+'''
+if Update()==False:
+    Update()
+'''
