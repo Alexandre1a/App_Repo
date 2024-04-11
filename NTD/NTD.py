d44bc6aba656a18d2f9c0d3ec24f0e893edff674
Alexandre1a <Mateialex@outlook.fr>
2024-04-11 09:33:07 +0200

    Update 0.4.1
    Started the App code, but i'm running out of time

--- NTD/NTD.py
+++ NTD/NTD.py
@@ -1,3 +1,42 @@
-import tkinter as tk
-from tkinter import ttk
-import nava
+import customtkinter as ctk
+from nava import *
+import time
+import threading
+import os
+
+# Set the colors
+ctk.set_appearance_mode("System")
+ctk.set_default_color_theme("blue")
+
+app = ctk.CTk()
+app.geometry("400x240")
+
+# Sets the default time
+initial_time = 0
+
+# Sets the start time
+start_time = None
+
+# Sets the status of the countdown
+is_running = False
+
+def update_countown():
+    global initial_time, start_time, is_running
+
+    # 
+
+def play_song():
+    sound_id = play("./Musics/text.wav", async_mode=True)
+    try:
+        sound_id = play("./Musics/text.wav", async_mode=True)
+    except NavaBaseError as e:
+        print(str(e))
+
+play_button = ctk.CTkButton(master= app, text="Play", command=play_song)
+play_button.place(relx=0.5, rely=0.5, anchor=ctk.SW)
+
+
+
+
+
+app.mainloop()
